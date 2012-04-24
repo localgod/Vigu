42a524160f9951ece51cb82a6a5e5ca0e0731801
Johannes Skov Frandsen <localgod@heaven.dk>
2012-04-24 12:59:28 +0200

    Updated docblock

--- Configuration.php
+++ Configuration.php
@@ -1,13 +1,16 @@
 <?php
 /**
- * TODO_DOCUMENT_ME
- *
+ * This file is part of the Vigu PHP error aggregation system.
+ * 
  * PHP version 5
- *
- * @category TODO_DOCUMENT_ME
- * @package  TODO_DOCUMENT_ME
- * @author   Jens Riisom Schultz <ibber_of_crew42@hotmail.com>
- * @since    2012-TODO-
+ * 
+ * @category  Application
+ * @package   Vigu 
+ * @author    Jens Riisom Schultz <ibber_of_crew42@hotmail.com>
+ * @author    Johannes Skov Frandsen <localgod@heaven.dk>
+ * @copyright 2012 Jens Riisom Schultz, Johannes Skov Frandsen
+ * @license   http://www.apache.org/licenses/LICENSE-2.0 Apache License, Version 2.0
+ * @link      https://github.com/Ibmurai/vigu
  */
 
 /**

6ae9f00f98cb5625733725c1b9e5a894a891a51a
Johannes Skov Frandsen <loalgod@heaven.dk>
2012-10-19 12:30:36 +0200

    Updated php doc

--- install.php
+++ install.php
@@ -2,10 +2,15 @@
 <?php
 /**
  * This file is part of the Vigu PHP error aggregation system.
- * @link https://github.com/localgod/Vigu
  *
+ * PHP version 5.3+
+ * 
+ * @category  ErrorAggregation
+ * @package   Vigu
+ * @author    Jens Riisom Schultz <ibber_of_crew42@hotmail.com>
  * @copyright 2012 Copyright Jens Riisom Schultz, Johannes Skov Frandsen
  * @license   http://www.apache.org/licenses/LICENSE-2.0 Apache License, Version 2.0
+ * @link      https://github.com/localgod/Vigu
  */
 set_time_limit(0);
 
